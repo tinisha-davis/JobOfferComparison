df25d844d83ff86f6e1c2b3996ce4ad861ef1c65
Jay A <Jenna.Arends@Seattlecolleges.edu>
2025-11-10 20:01:48 -0800

    Began side by side display component

--- my-react-app/src/components/FieldContainer.jsx
+++ my-react-app/src/components/FieldContainer.jsx
@@ -3,7 +3,15 @@ import Field from './Field'
 export default function FieldContainer( { dataFields } ) {
 
     return (
-        <section class="fieldContainer">
+        <section className="fieldContainer">
+
+            <div className="offerPlacehold">
+                <p>Offer Document Will Display Here</p>
+                {/*upload logic here*/}
+            </div>
+
+            <div>     </div>
+
             {dataFields.map(dataField => (
                 <Field key={dataField.id} dataField={dataField} />
             ))}
